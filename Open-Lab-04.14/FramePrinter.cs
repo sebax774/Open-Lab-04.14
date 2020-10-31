98d2e241d2bbdd6c3ad941921a4f13c437ec7881
sebax774 <bugansebastian@gmail.com>
2020-10-31 14:28:53 +0100

    yeah

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,38 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string frame = "";
+            int hey = 0;
+
+            foreach (string sup in strings)
+            {
+                if (hey < sup.Length)
+                    hey = sup.Length;
+            }
+
+            for (int i = 0; i < hey + 4; i++)
+            {
+                frame += "*";
+            }
+
+            Console.WriteLine(frame);
+
+            foreach (string sup in strings)
+            {
+                if (sup.Length < hey)
+                {
+                    string str = "";
+                    for (int i = 0; i < hey - sup.Length; i++)
+                    {
+                        str += " ";
+                    }
+                    Console.WriteLine("* " + sup + str + " *");
+                }
+                else
+                    Console.WriteLine("* " + sup + " *");
+            }
+
+            Console.WriteLine(frame);
         }
     }
 }
